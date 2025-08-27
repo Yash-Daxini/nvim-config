725835756861830477c543ab8b6131931593659c
infinity <yashdaxini2003@gmail.com>
2025-08-27 21:51:55 +0530

    Configure full size nvim window in kitty

--- init.lua
+++ init.lua
@@ -11,6 +11,24 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+        local id = os.getenv("KITTY_WINDOW_ID")
+        if id then
+            os.execute("kitty @ --to $KITTY_LISTEN_ON set-spacing --match id:"..id.." padding=0")
+        end
+    end,
+})
+
+vim.api.nvim_create_autocmd("VimLeavePre", {
+    callback = function()
+        local id = os.getenv("KITTY_WINDOW_ID")
+        if id then
+            os.execute("kitty @ --to $KITTY_LISTEN_ON set-spacing --match id:"..id.." padding=25")
+        end
+    end,
+})
+
 require("vim-options")
 require("lazy").setup("plugins")
 
